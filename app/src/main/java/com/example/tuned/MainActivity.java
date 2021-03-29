e438545bdcb5e416e7799bbf76010374d98dbd2f
Adriana <abelt023@fiu.edu>
2021-03-28 22:52:41 -0400

    Added bottom navigation bar & fragments

--- app/src/main/java/com/example/tuned/MainActivity.java
+++ app/src/main/java/com/example/tuned/MainActivity.java
@@ -1,14 +1,62 @@
 package com.example.tuned;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.tuned.fragments.DiscoverFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    final FragmentManager fragmentManager = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+
+                switch (item.getItemId()) {
+                    case R.id.action_discover:
+                        Toast.makeText(MainActivity.this, "Discover!", Toast.LENGTH_SHORT).show();
+                        fragment = new DiscoverFragment();
+                        break;
+                    case R.id.action_search:
+                        Toast.makeText(MainActivity.this, "Search!", Toast.LENGTH_SHORT).show();
+                        fragment = new DiscoverFragment();
+                        break;
+                    case R.id.action_create:
+                        Toast.makeText(MainActivity.this, "Create Review!", Toast.LENGTH_SHORT).show();
+                        fragment = new DiscoverFragment();
+                        break;
+                    case R.id.action_alerts:
+                        Toast.makeText(MainActivity.this, "Alerts!", Toast.LENGTH_SHORT).show();
+                        fragment = new DiscoverFragment();
+                        break;
+                    case R.id.action_profile:
+                    default:
+                        Toast.makeText(MainActivity.this, "Profile!", Toast.LENGTH_SHORT).show();
+                        fragment = new DiscoverFragment();
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_discover);
     }
 }
