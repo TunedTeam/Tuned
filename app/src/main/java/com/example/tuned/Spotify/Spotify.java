63af50a1efa0ebbd3d64a5831487ac895210b5e1
Adriana <abelt023@fiu.edu>
2021-04-05 03:30:12 -0400

    Added Spotify API implementation

---
+++ app/src/main/java/com/example/tuned/Spotify/Spotify.java
@@ -0,0 +1,15 @@
+package com.example.tuned.Spotify;
+
+import com.adamratzman.spotify.SpotifyApiBuilderKt;
+import com.adamratzman.spotify.SpotifyAppApi;
+
+public class Spotify{
+    private static SpotifyAppApi instance;
+
+    public static SpotifyAppApi getSpotifyApi () {
+        if (Spotify.instance == null) {
+            Spotify.instance = (SpotifyAppApi) SpotifyApiBuilderKt.spotifyAppApi("clientId", "clientSecret").build(true, null);
+        }
+        return Spotify.instance;
+    }
+}
